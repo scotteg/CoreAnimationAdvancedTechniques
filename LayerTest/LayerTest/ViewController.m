6b3f487890e0547195118ed64f1f9456c46c9f66
Scott Gardner <scott.gardner@mac.com>
2014-03-28 07:21:46 -0500

    Chapter 2: Custom Drawing

--- LayerTest/LayerTest/ViewController.m
+++ LayerTest/LayerTest/ViewController.m
@@ -10,12 +10,7 @@
 @import QuartzCore;
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIView *coneView;
-@property (weak, nonatomic) IBOutlet UIView *shipView;
-@property (weak, nonatomic) IBOutlet UIView *iglooView;
-@property (weak, nonatomic) IBOutlet UIView *anchorView;
-@property (weak, nonatomic) IBOutlet UIView *button1View;
-@property (weak, nonatomic) IBOutlet UIView *button2View;
+@property (weak, nonatomic) IBOutlet UIView *layerView;
 @end
 
 @implementation ViewController
@@ -24,39 +19,20 @@
 {
   [super viewDidLoad];
   
-  UIImage *sprite = [UIImage imageNamed:@"Sprites"];
-  [self addSpriteImage:sprite withContentRect:CGRectMake(0.0f, 0.0f, 0.5f, 0.5f) toLayer:self.iglooView.layer];
-  [self addSpriteImage:sprite withContentRect:CGRectMake(0.5f, 0.0f, 0.5f, 0.5f) toLayer:self.coneView.layer];
-  [self addSpriteImage:sprite withContentRect:CGRectMake(0.0f, 0.5f, 0.5f, 0.5f) toLayer:self.anchorView.layer];
-  [self addSpriteImage:sprite withContentRect:CGRectMake(0.5f, 0.5f, 0.5f, 0.5f) toLayer:self.shipView.layer];
-  self.coneView.hidden = YES;
-  self.shipView.hidden = YES;
-  self.iglooView.hidden = YES;
-  self.anchorView.hidden = YES;
-  
-  UIImage *button = [UIImage imageNamed:@"Button-green"];
-  
-  // contentCenter can also be set in storyboard View > Attributes > Stretching
-  // http://my.safaribooksonline.com/getfile?item=NC9hN2RyYzg5c3B0bWcwaS8vczFzM2U0NDc0MGEzLmgyZXN0ZjAvZ3BpaXJhL3NnMWpjZ3Ax
-  [self addStretchableImage:button withContentCenter:CGRectMake(0.25f, 0.25f, 0.5f, 0.5f) toLayer:self.button1View.layer];
-  [self addStretchableImage:button withContentCenter:CGRectMake(0.25f, 0.25f, 0.5f, 0.5f) toLayer:self.button2View.layer];
+  CALayer *blueLayer = [CALayer layer];
+  blueLayer.frame = CGRectMake(50.0f, 50.0f, 100.0f, 100.0f);
+  blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+  blueLayer.delegate = self;
+  blueLayer.contentsScale = [UIScreen mainScreen].scale;
+  [self.layerView.layer addSublayer:blueLayer];
+  [blueLayer display]; // Force redraw to display red circle
 }
 
-- (void)addSpriteImage:(UIImage *)image withContentRect:(CGRect)rect toLayer:(CALayer *)layer
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx
 {
-  layer.contents = (__bridge id)image.CGImage;
-  layer.contentsGravity = kCAGravityResizeAspect;
-  layer.contentsRect = rect;
-  
-//  layer.contentsGravity = kCAGravityCenter;
-//  layer.contentsScale = [UIScreen mainScreen].scale; // Or set to image.scale
-//  layer.masksToBounds = YES;
-}
-
-- (void)addStretchableImage:(UIImage *)image withContentCenter:(CGRect)rect toLayer:(CALayer *)layer
-{
-  layer.contents = (__bridge id)image.CGImage;
-  layer.contentsCenter = rect;
+  CGContextSetLineWidth(ctx, 10.0f);
+  CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+  CGContextStrokeEllipseInRect(ctx, CGRectInset(layer.bounds, 5.0f, 5.0f));
 }
 
 @end
