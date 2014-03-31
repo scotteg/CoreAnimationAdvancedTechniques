3a1553bc442b24c27496dd1edfe026499128888d
Scott Gardner <scott.gardner@mac.com>
2014-03-31 07:55:09 -0500

    Chapter 8: UIKit Transition

--- AnimationTest/AnimationTest/TransitionViewController.m
+++ AnimationTest/AnimationTest/TransitionViewController.m
@@ -30,21 +30,38 @@
 
 - (IBAction)switchImageButtonTapped:(id)sender
 {
-  CATransition *transition = [CATransition animation]; // Operates on whole layer, not a specific property
+//  CATransition *transition = [CATransition animation]; // Operates on whole layer, not a specific property
+//  
+//  NSArray *types = @[kCATransitionFade, kCATransitionMoveIn, kCATransitionPush, kCATransitionReveal];
+//  NSArray *subtypes = @[kCATransitionFromBottom, kCATransitionFromLeft, kCATransitionFromRight, kCATransitionFromTop];
+//  
+//  transition.type = types[arc4random() % 4];
+//  transition.subtype = subtypes[arc4random() % 4];
+//  
+//  // Unlike property animations, only 1 transition can be operate on a layer at a time, so the key is automatically set to "transition" (kCATransition) regardless of what is passed for key
+//  [self.imageView.layer addAnimation:transition forKey:nil];
+//  
+//  UIImage *currentImage = self.imageView.image;
+//  NSUInteger index = [self.images indexOfObject:currentImage];
+//  index = (index + 1) % [self.images count];
+//  self.imageView.image = self.images[index];
   
-  NSArray *types = @[kCATransitionFade, kCATransitionMoveIn, kCATransitionPush, kCATransitionReveal];
-  NSArray *subtypes = @[kCATransitionFromBottom, kCATransitionFromLeft, kCATransitionFromRight, kCATransitionFromTop];
+  // Using UIKit instead
+  __weak TransitionViewController *weakSelf = self;
+  NSArray *options = @[@(UIViewAnimationOptionTransitionFlipFromBottom),
+                       @(UIViewAnimationOptionTransitionFlipFromLeft),
+                       @(UIViewAnimationOptionTransitionFlipFromRight),
+                       @(UIViewAnimationOptionTransitionFlipFromTop)];
   
-  transition.type = types[arc4random() % 4];
-  transition.subtype = subtypes[arc4random() % 4];
+  NSUInteger option = [options[arc4random() % 4] integerValue];
   
-  // Unlike property animations, only 1 transition can be operate on a layer at a time, so the key is automatically set to "transition" (kCATransition) regardless of what is passed for key
-  [self.imageView.layer addAnimation:transition forKey:nil];
-  
-  UIImage *currentImage = self.imageView.image;
-  NSUInteger index = [self.images indexOfObject:currentImage];
-  index = (index + 1) % [self.images count];
-  self.imageView.image = self.images[index];
+  [UIView transitionWithView:self.imageView duration:1.0 options:option animations:^{
+    // Cycle to next image
+    UIImage *currentImage = self.imageView.image;
+    NSUInteger index = [self.images indexOfObject:currentImage];
+    index = (index + 1) % [self.images count];
+    weakSelf.imageView.image = self.images[index];
+  } completion:nil];
 }
 
 @end
