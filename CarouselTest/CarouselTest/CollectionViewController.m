54c7dc7c4945183c1642f5b97265794f5852751b
Scott Gardner <scott.gardner@mac.com>
2014-04-04 07:46:05 -0500

    Chapter 14: Adding Caching and Speculative Loading

--- CarouselTest/CarouselTest/CollectionViewController.m
+++ CarouselTest/CarouselTest/CollectionViewController.m
@@ -8,8 +8,6 @@
 
 #import "CollectionViewController.h"
 
-static NSUInteger const kImageTag = 99;
-
 @interface CollectionViewController ()
 @property (copy, nonatomic) NSArray *imagePaths;
 @end
@@ -23,8 +21,51 @@ static NSUInteger const kImageTag = 99;
   self.imagePaths = [[NSBundle mainBundle] pathsForResourcesOfType:@"png" inDirectory:@"Vacation Photos"];
 }
 
+- (UIImage *)loadImageAtIndex:(NSUInteger)index
+{
+  static NSCache *cache = nil;
+  
+  if (!cache) {
+    cache = [NSCache new];
+  }
+  
+  // Return if already cached
+  UIImage *image = [cache objectForKey:@(index)];
+  if (image) {
+    return [image isKindOfClass:[NSNull class]] ? nil : image;
+  }
+  
+  // Set placeholder to avoid reloading image multiple times
+  [cache setObject:[NSNull null] forKey:@(index)];
+  
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+    NSString *imagePath = self.imagePaths[index];
+    UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
+    
+    // Redraw image using device context
+    UIGraphicsBeginImageContextWithOptions(image.size, YES, 0.0f);
+    [image drawAtPoint:CGPointZero];
+    image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    // Set image for correct image view
+    dispatch_async(dispatch_get_main_queue(), ^{
+      [cache setObject:image forKey:@(index)];
+      
+      NSIndexPath *indexPath = [NSIndexPath indexPathForItem:index inSection:0];
+      UICollectionViewCell *cell = [self.collectionView cellForItemAtIndexPath:indexPath];
+      UIImageView *imageView = [cell.contentView.subviews lastObject];
+      imageView.image = image;
+    });
+  });
+  
+  // Not loaded yet
+  return nil;
+}
+
 #pragma mark - UICollectionViewDataSource
 
+// Use NSCache and speculative loading; results in slightly degraded initial scrolling performance compared to using CATiledLayer, but preloading could be improved by taking into account scroll speed and direction
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
   return [self.imagePaths count];
@@ -33,37 +74,26 @@ static NSUInteger const kImageTag = 99;
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
   UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+  NSInteger item = indexPath.item;
   
-  UIImageView *imageView = (UIImageView *)[cell viewWithTag:kImageTag];
+  UIImageView *imageView = [cell.contentView.subviews lastObject];
+  if (!imageView) {
+    imageView = [[UIImageView alloc] initWithFrame:cell.contentView.bounds];
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    [cell.contentView addSubview:imageView];
+  }
   
-//  NSInteger row = indexPath.row;
-//  NSString *imagePath = self.imagePaths[row];
-//  UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
-//  imageView.image = image;
+  // Set or load image for this index
+  imageView.image = [self loadImageAtIndex:item];
   
-  // Force image decompression prior to display
-  cell.tag = indexPath.row;
-  imageView.image = nil;
+  // Preload image for previous and next indexes
+  if (indexPath.item < [self.imagePaths count] - 1) {
+    [self loadImageAtIndex:item + 1];
+  }
   
-  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    // Load image
-    NSInteger row = indexPath.row;
-    NSString *imagePath = self.imagePaths[row];
-    UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
-    
-    // Redraw image using device context
-    UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, YES, 0.0f);
-    [image drawInRect:imageView.bounds];
-    image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    // Set image on main thread if row still matches
-    dispatch_async(dispatch_get_main_queue(), ^{
-      if (row == cell.tag) {
-        imageView.image = image;
-      }
-    });
-  });
+  if (indexPath.item > 0) {
+    [self loadImageAtIndex:item - 1];
+  }
   
   return cell;
 }
