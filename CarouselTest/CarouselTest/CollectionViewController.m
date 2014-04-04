fbd02165d36e4fc5322e017770ec9d60d18945ce
Scott Gardner <scott.gardner@mac.com>
2014-04-04 07:14:54 -0500

    Chapter 14: CATiledLayer

--- CarouselTest/CarouselTest/CollectionViewController.m
+++ CarouselTest/CarouselTest/CollectionViewController.m
@@ -8,8 +8,6 @@
 
 #import "CollectionViewController.h"
 
-static NSUInteger const kImageTag = 99;
-
 @interface CollectionViewController ()
 @property (copy, nonatomic) NSArray *imagePaths;
 @end
@@ -34,38 +32,50 @@ static NSUInteger const kImageTag = 99;
 {
   UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
   
-  UIImageView *imageView = (UIImageView *)[cell viewWithTag:kImageTag];
+  CATiledLayer *tileLayer = [cell.contentView.layer.sublayers lastObject];
   
-//  NSInteger row = indexPath.row;
-//  NSString *imagePath = self.imagePaths[row];
-//  UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
-//  imageView.image = image;
-  
-  // Force image decompression prior to display
-  cell.tag = indexPath.row;
-  imageView.image = nil;
-  
-  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-    // Load image
-    NSInteger row = indexPath.row;
-    NSString *imagePath = self.imagePaths[row];
-    UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
+  if (!tileLayer) {
+    CGFloat scale = [UIScreen mainScreen].scale;
+    tileLayer = [CATiledLayer layer];
+    tileLayer.frame = cell.bounds;
     
-    // Redraw image using device context
-    UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, YES, 0.0f);
-    [image drawInRect:imageView.bounds];
-    image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
+    // The tileSize property of CATiledLayer is measured in pixels, not points, so to ensure that the tile exactly matches the size of the cell, multiply the size by the screen scale
+    tileLayer.contentsScale = scale;
+    tileLayer.tileSize = CGSizeMake(CGRectGetWidth(cell.bounds) * scale, CGRectGetHeight(cell.bounds) * scale);
+    tileLayer.delegate = self;
     
-    // Set image on main thread if row still matches
-    dispatch_async(dispatch_get_main_queue(), ^{
-      if (row == cell.tag) {
-        imageView.image = image;
-      }
-    });
-  });
+    // Tag each layer using CALayer's KVC ability to store arbitrary values
+    [tileLayer setValue:@(indexPath.row) forKeyPath:@"index"];
+    
+    [cell.contentView.layer addSublayer:tileLayer];
+  }
+  
+  // Tag layer with correct index and reload
+  tileLayer.contents = nil;
+  [tileLayer setValue:@(indexPath.row) forKeyPath:@"index"];
+  [tileLayer setNeedsDisplay];
   
   return cell;
+}
+
+/* CATiledLayer calls -drawLayer:inContext: method for each tile concurrently on multiple threads, which avoids blocking the user interface and takes advantage of multiple processor cores for faster tile drawing. A CATiledLayer with just a single tile is a cheap way to implement an asynchronously-updating image view.
+ */
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx
+{
+  NSInteger index = [[layer valueForKey:@"index"] integerValue];
+  
+  NSString *imagePath = self.imagePaths[index];
+  UIImage *tileImage = [UIImage imageWithContentsOfFile:imagePath];
+  
+  CGFloat aspectRatio = tileImage.size.height / tileImage.size.width;
+  CGRect imageRect = CGRectZero;
+  imageRect.size.width = CGRectGetWidth(layer.bounds);
+  imageRect.size.height = CGRectGetHeight(layer.bounds) * aspectRatio;
+  imageRect.origin.y = (CGRectGetHeight(layer.bounds) - CGRectGetHeight(imageRect)) / 2.0f;
+  
+  UIGraphicsPushContext(ctx);
+  [tileImage drawInRect:imageRect];
+  UIGraphicsPopContext();
 }
 
 @end
